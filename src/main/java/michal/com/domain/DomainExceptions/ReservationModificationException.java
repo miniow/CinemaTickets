4edee18ebb3b606c1cda8304549728f8184a34e3
miniow <zymcioxx@gmail.com>
2025-04-30 10:13:37 +0200

    init

---
+++ src/main/java/michal/com/domain/DomainExceptions/ReservationModificationException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package michal.com.domain.DomainExceptions;
+
+/**
+ *
+ * @author zymci
+ */
+public class ReservationModificationException extends DomainException {
+    public ReservationModificationException(String message) {
+        super(message);
+    }
+}
